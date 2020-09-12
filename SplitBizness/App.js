9dbe0656ca57759e39246a4630066b0c1ab4d74a
chiliwa <34013228+chiliwax@users.noreply.github.com>
2020-09-12 20:05:10 +0200

    QrCode example

--- SplitBizness/App.js
+++ SplitBizness/App.js
@@ -8,18 +8,35 @@
 
 import React from 'react';
 
+/**
+ * THEMING
+ */
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import {ApplicationProvider, Layout, Text} from '@ui-kitten/components';
+
+/**
+ * QrCode
+ */
+import QRCode from 'react-native-qrcode-svg';
+
+var segs = [
+  { data: 'ABCDEFG', mode: 'alphanumeric' },
+  { data: '0123456', mode: 'numeric' }
+]
+
+
 
 const HomeScreen = () => (
   <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
     <Text category="h1">HOME</Text>
+    <QRCode value={segs} />
   </Layout>
 );
 
 const App = () => {
+  
   return (
-    <ApplicationProvider {...eva} theme={eva.dark}>
+    <ApplicationProvider {...eva} theme={eva.light}>
       <HomeScreen />
     </ApplicationProvider>
   );
