f18a39a5710ecbb72679d7d70325a037c5028d4f
chiliwa <34013228+chiliwax@users.noreply.github.com>
2020-09-12 22:55:43 +0200

    add Split Logo for QRCode OEM

--- SplitBizness/App.js
+++ SplitBizness/App.js
@@ -19,19 +19,33 @@ import {ApplicationProvider, Layout, Text} from '@ui-kitten/components';
  */
 import QRCode from 'react-native-qrcode-svg';
 
+/**
+ * import and create QrCodeLogo
+ */
+import logo from './public/images/Logo_Split_QRCODE.png';
+
 let segs = {
   token: 'secret-weapon',
-  receiver: 'resto@paypal.fr', 
-  amount: 10.5, 
+  receiver: 'resto@paypal.fr',
+  amount: 10.5,
   weblink: 'http://chiliwa.me',
-}
+  deeplink: 'fb://chiliwaa',
+};
 
-segs = JSON.stringify(segs)
+segs = JSON.stringify(segs);
 
 const HomeScreen = () => (
   <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category="h1">HOME</Text>
-    <QRCode size={300} value={segs} />
+    <Text category="h1">PAYMENT</Text>
+    <QRCode
+      size={300}
+      value={segs}
+      logo={logo}
+      logoSize={50}
+      logoBackgroundColor="white"
+      quietZone={1}
+      logoMargin={0}
+    />
   </Layout>
 );
 
